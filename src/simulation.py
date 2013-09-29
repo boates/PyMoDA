b2feb91536fa6a65769a76c906fe9c14b42b3555
Brian Boates <boates@squareup.com>
2013-09-29 11:45:29 -0700

    simulation object

---
+++ src/simulation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+simulation.py
+Author: Brian Boates
+
+Implements Simulation()
+"""
+import numpy as np
+from atom import Atom
+from lattice import Lattice
+from configuration import Configuration
+
+class Simulation(object):
+    """
+    """
+    def __init__(self):
+        """
+        """
+        pass
+
+    def __str__(self):
+        """
+        return: string
+        """
+        s = '<Simulation>'
+        return s
+
+    def __repr__(self):
+        """
+        return: string
+        """
+        return self.__str__()
+
+    def insert_configuration(configuration):
+        """
+        """
+        pass
